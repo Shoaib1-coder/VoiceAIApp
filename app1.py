7b38a3dbaa08798f42154a1fb88b21c37abbf38a
Shoaibrai1 <mshoaib3393@gmail.com>
2025-04-30 03:19:21 -0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -159,15 +159,15 @@ def show_home():
 
 
 
- def show_login():
-     st.title(" Login")
+def show_login():
+    st.title(" Login")
 
-     username = st.text_input("Username")
-     password = st.text_input("Password", type="password")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
 
-     if st.button("Login"):
-         user = #login_user(username, password)
-         if user:
+    if st.button("Login"):
+      user = #login_user(username, password)
+        if user:
              st.success("Login Successful!")
              st.session_state.logged_in = True
              st.session_state.page = "Home"
