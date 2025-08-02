0dc4113ae0fcd29dad3cc0ac364067650be0828f
Muhammad Shoaib <mshoaib3393@gmail.com>
2025-08-01 18:59:13 -0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -2,7 +2,6 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from voice_changer import main as voice_changer_main
 from voice_isolator import main as voice_isolator_main
-from sound_effects import main as sound_effects_main
 from speech_to_text import main as speech_to_text
 from text_to_speech import main as tts_main
 from gtts import gTTS
@@ -46,7 +45,7 @@ def show_tools():
     st.sidebar.title("AI Voice Toolkit")
     selected_page = st.sidebar.radio(
         "Navigate",
-        [ "Text to Speech", "Speech to Text", "Voice Changer", "Sound Effects", "Voice Isolator"]
+        [ "Text to Speech", "Speech to Text", "Voice Changer","Voice Isolator"]
     )
 
     
@@ -57,8 +56,7 @@ def show_tools():
         speech_to_text()
     elif selected_page == "Voice Changer":
         voice_changer_main()
-    elif selected_page == "Sound Effects":
-        sound_effects_main()
+
     elif selected_page == "Voice Isolator":
         voice_isolator_main()
  
@@ -70,7 +68,6 @@ def show_documentation():
     - **Text to Speech**: Convert your text into human-like voice.
     - **Speech to Text**: Convert spoken audio into written text.
     - **Voice Changer**: Modify your voice pitch and tone.
-    - **Sound Effects**: Add background sound effects to your recordings.
     - **Voice Isolator**: Remove noise and isolate voice clearly.
    
 
